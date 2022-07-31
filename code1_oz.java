6c82d54934a1a549745ce687047358e05f620b3a
Kaumil2003 <110337269+Kaumil2003@users.noreply.github.com>
2022-07-31 20:00:12 +0530

    Add files via upload

---
+++ code1_oz.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class code1_oz {
+    public static void main(String[] args) {
+        String s;
+        Scanner str = new Scanner(System.in);
+        s = sc.next();
+       char[] ch = s.toCharArray();// converting the string into array of char
+
+
+
+        if(ch[0] == 'o')
+        {
+            System.out.print(ch[0]);
+        }
+        if(ch[1] == 'z')
+        {
+            System.out.print(ch[1]);
+
+        }
+        System.out.println();
+    }
+
+}
+
+
+
